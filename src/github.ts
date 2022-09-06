d15d2dd0137599db0a41caa118e740cb3d2ec2a2
Dimitri Lavrenük <dimitri.lavrenuek@camunda.com>
2022-09-06 17:13:59 +0200

    Revert "chore: remove p-limit"
    This reverts commit 38269d76edf6cf7479339793ac4f80388b3afab2.

--- src/github.ts
+++ src/github.ts
@@ -1,60 +1,68 @@
 import { Octokit } from '@octokit/rest';
 import github from '@actions/github';
 import { info } from '@actions/core';
+import pLimit from 'p-limit';
 
 const octokit = new Octokit({ auth: `token ${process.env.GITHUB_TOKEN}` });
 const { owner, repo } = github.context.repo;
+const requestLimit = pLimit(10);
 
 export const loadPullRequests = async (ids: number[]) =>
   (
     await Promise.all(
-      ids.map(async (id) => {
-        try {
-          const pr = await octokit.pulls.get({
-            owner,
-            repo,
-            pull_number: id,
-          });
+      ids.map((id) =>
+        requestLimit(async () => {
+          try {
+            const pr = await octokit.pulls.get({
+              owner,
+              repo,
+              pull_number: id,
+            });
 
-          if (pr.status !== 200) {
+            if (pr.status !== 200) {
+              return null;
+            }
+
+            return pr.data.body;
+          } catch (e) {
+            info(
+              `Retrieving pull request with id "${id}" failed with the message: ${e}`
+            );
             return null;
           }
-
-          return pr.data.body;
-        } catch (e) {
-          info(
-            `Retrieving pull request with id "${id}" failed with the message: ${e}`
-          );
-          return null;
-        }
-      })
+        })
+      )
     )
   ).filter((pr): pr is string => pr !== null);
 
 export const ensureLabelsExist = async (labels: string[], color: string) =>
   Promise.all(
-    labels.map(async (name) => {
-      try {
-        await octokit.issues.createLabel({
-          owner,
-          repo,
-          color,
-          name,
-        });
-      } catch (e) {
-        info(`Label ${name} could not be created, reason: ${e}`);
-      }
-    })
+    labels.map((name) =>
+      requestLimit(async () => {
+        try {
+          await octokit.issues.createLabel({
+            owner,
+            repo,
+            color,
+            name,
+          });
+        } catch (e) {
+          info(`Label ${name} could not be created, reason: ${e}`);
+        }
+      })
+    )
   );
 
 export const addLabels = async (issueIds: number[], labels: string[]) =>
   Promise.all(
     issueIds.map((id) =>
-      octokit.issues.addLabels({
-        owner,
-        repo,
-        issue_number: id,
-        labels,
-      })
+      requestLimit(() =>
+        octokit.issues.addLabels({
+          owner,
+          repo,
+          issue_number: id,
+          labels,
+        })
+      )
     )
   );
